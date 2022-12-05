f5f3c55eb7a81fe9aaf1aecb905ef29f1d7864bb
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-12-05 12:10:36 +0500

    in progress

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 class Link < ApplicationRecord
   validates :lookup_code, :original_url, presence: true
   validates :lookup_code, uniqueness: true
+  validate :original_url_format
+
+  def original_url_format
+    uri = URI.parse(original_url || "")
+    if uri.host.nil?
+      errors.add(:original_url, "Invalid URL format")
+    end
+  end
+
+  def shortened_url
+    "http://localhost/#{lookup_code}"
+  end
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe LinksController, type: :controller do
 
   it 'can shorten a link provided by a user' do
     url = 'https://gitlab.com/upay.net/uzpaynet/-/tree/master/app/services'
-    headers = { "CONTENT_TYPE" => "text/javascript" }
+
+    request.env['HTTP_ACCEPT'] = 'text/javascript'
     # byebug
     post :create, params: { link: { original_url: url } }
     link = assigns(:link)
     expect(link.original_url).to eq(url)
     expect(link.valid?).to be(true)
     expect(link.lookup_code.length).to eq(7)
-
+    expect(link.persisted?).to be(true)
+    expect(response).to render_template('create')
   end
 
 end
